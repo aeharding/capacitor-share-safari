1bf28b08d823f8709622760a4b0f6c301172aa58
Alexander Harding <noreply@harding.dev>
2025-02-03 23:56:13 -0600

    Fix share on iPad (#1)

--- ios/Sources/ShareSafariPlugin/ShareSafariPlugin.swift
+++ ios/Sources/ShareSafariPlugin/ShareSafariPlugin.swift
@@ -12,7 +12,6 @@ public class ShareSafariPlugin: CAPPlugin, CAPBridgedPlugin {
     public let pluginMethods: [CAPPluginMethod] = [
         CAPPluginMethod(name: "share", returnType: CAPPluginReturnPromise)
     ]
-    private let implementation = ShareSafari()
 
     @objc func share(_ call: CAPPluginCall) {
         guard let url = call.getString("url") else {
@@ -20,15 +19,38 @@ public class ShareSafariPlugin: CAPPlugin, CAPBridgedPlugin {
             return
         }
 
-        DispatchQueue.main.async {
-            if let viewController = self.bridge?.viewController {
-                self.implementation.share(url, from: viewController)
-                call.resolve([
-                    "status": "success"
-                ])
-            } else {
-                call.reject("Unable to get view controller")
+        let safariItem = MyActivity(title: "Open in Safari", image: UIImage(systemName: "safari")  ) { sharedItems in
+             guard let url = sharedItems[0] as? URL else { return }
+             UIApplication.shared.open(url)
+         }
+
+        let items = [URL(string: url)!]
+
+        DispatchQueue.main.async { [weak self] in
+            let actionController = UIActivityViewController(activityItems: items, applicationActivities: [safariItem])
+
+            actionController.completionWithItemsHandler = { (activityType, completed, _ returnedItems, activityError) in
+                if activityError != nil {
+                    call.reject("Error sharing item", nil, activityError)
+                    return
+                }
+
+                if completed {
+                    call.resolve([
+                        "activityType": activityType?.rawValue ?? ""
+                    ])
+                } else {
+                    call.reject("Share canceled")
+                }
+
             }
+            if self?.bridge?.viewController?.presentedViewController != nil {
+                call.reject("Can't share while sharing is in progress")
+                return
+            }
+            self?.setCenteredPopover(actionController)
+            self?.bridge?.viewController?.present(actionController, animated: true, completion: nil)
         }
+
     }
 }
